3972667fc84a87d1893b433102c81531578f7936
Kaan Kani İnce <kkince@newmind.ai>
2024-07-29 13:30:06 +0300

    deleteTodo created

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -1,6 +1,68 @@
+import {useEffect, useState} from "react";
+
 const ListTodos = () => {
-    return(
+
+    const [todos, setTodos] = useState([]);
+
+    const deleteTodo = async (id) => {
+        try {
+            const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+                method: "DELETE"
+            });
+
+            setTodos(todos.filter(todo => todo.todo_id !== id))
+        }catch (err) {
+            console.error(err.message)
+        }
+    }
+
+    const getTodos = async () => {
+        try {
+            const response = await fetch("http://localhost:5000/todos")
+            const jsonData = await response.json();
+
+            setTodos(jsonData);
+        } catch (err) {
+            console.error(err.message)
+        }
+    }
+
+    useEffect(() => {
+        getTodos();
+    }, []);
+
+    return (
         <>
+            <table class="table mt-5 text-center">
+                <thead>
+                <tr>
+                    <th>Description</th>
+                    <th>Edit</th>
+                    <th>Delete</th>
+                </tr>
+                </thead>
+                <tbody>
+                {/*<tr>
+                    <td>John</td>
+                    <td>Doe</td>
+                    <td>john@example.com</td>
+                </tr>*/}
+                {todos.map(todo => (
+                    <tr key={todo.todo_id}>
+                        <td>{todo.description}</td>
+                        <td>
+                            <button className="btn btn-warning">Delete</button>
+                        </td>
+                        <td>
+                            <button className="btn btn-danger"
+                                    onClick={() => deleteTodo(todo.todo_id)}>
+                                Delete
+                            </button>
+                        </td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
         </>
     )
 }
